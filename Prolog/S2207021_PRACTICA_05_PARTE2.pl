bfc99d46fcd51e95c6b20d814d6fc64add5e09d3
Sinhularity <zs22107021@estudiantes.uv.mx>
2024-10-30 20:43:55 -0600

    Practica cinco parte 2

---
+++ Prolog/S2207021_PRACTICA_05_PARTE2.pl
@@ -0,0 +1,160 @@
+// :- initialization(main).
+// main :- write('Hello, World!').
+
+// Materno - Abuelos, Ti@s, Primos, etcétera...
+progenitor('Alejandro Garcia','Mateo Garcia').
+progenitor('Valentina Garcia', 'Mateo Garcia').
+
+progenitor('Mateo Garcia', 'Griselda Garcia').
+progenitor('Isabella Escobar', 'Griselda Garcia').
+
+progenitor('Mateo Garcia', 'Estrella Garcia').
+progenitor('Isabella Escobar', 'Estrella Garcia').
+
+progenitor('Mateo Garcia', 'Espinoza Garcia').
+progenitor('Isabella Escobar', 'Espinoza Garcia').
+
+progenitor('Mateo Garcia', 'Patricia Garcia').
+progenitor('Isabella Escobar', 'Patricia Garcia').
+
+progenitor('Mateo Garcia', 'Rosa Garcia').
+progenitor('Isabella Escobar', 'Rosa Garcia').
+
+progenitor('Pablo Nazaret', 'Guadalupe Nazaret').
+progenitor('Espinoza Garcia', 'Guadalupe Nazaret').
+
+progenitor('Pablo Nazaret', 'Guillermo Nazaret').
+progenitor('Espinoza Garcia', 'Guillermo Nazaret').
+
+progenitor('Luis Sanchez', 'Sofia Sanchez').
+progenitor('Estrella Garcia', 'Sofia Sanchez').
+
+progenitor('Alejandro Camacho', 'Alejandra Camacho').
+progenitor('Patricia Garcia', 'Alejandra Camacho').
+
+progenitor('Alejandro Camacho', 'Pamela Camacho').
+progenitor('Patricia Garcia', 'Pamela Camacho').
+
+progenitor('Alejandro Camacho', 'Luz Camacho').
+progenitor('Patricia Garcia', 'Luz Camacho').
+
+progenitor('Alejandro Camacho', 'Yutzil Camacho').
+progenitor('Patricia Garcia', 'Yutzil Camacho').
+
+progenitor('Josue Elvira', 'Yuliana Elvira').
+progenitor('Alejandra Camacho', 'Yuliana Elvira').
+
+progenitor('Josue Elvira', 'Alexa Elvira').
+progenitor('Alejandra Camacho', 'Alexa Elvira').
+
+progenitor('Miguel Altamirano', 'Sofia Altamirano').
+progenitor('Pamela Camacho', 'Sofia Altamirano').
+
+progenitor('Miguel Altamirano', 'Miguel Jr. Altamirano').
+progenitor('Pamela Camacho', 'Miguel Jr. Altamirano').
+
+// Paterno - Abuelos, Ti@s, Primos, etcétera...
+progenitor('Maximiliano Frias', 'Tomas Frias').
+progenitor('Elena Vega', 'Tomas Frias').
+
+progenitor('Maximiliano Frias', 'Fernando Frias').
+progenitor('Elena Vega', 'Fernando Frias').
+
+progenitor('Tomas Frias', 'Sebastian Frias').
+progenitor('Veronica Mendoza', 'Sebastian Frias').
+
+progenitor('Tomas Frias', 'Jesus Frias').
+progenitor('Veronica Mendoza', 'Jesus Frias').
+
+progenitor('Tomas Frias', 'Alvaro Frias').
+progenitor('Veronica Mendoza', 'Alvaro Frias').
+
+progenitor('Fernando Frias', 'Bartolo Frias').
+progenitor('Clara Molina', 'Bartolo Frias').
+
+progenitor('Fernando Frias', 'Toribio Frias').
+progenitor('Clara Molina', 'Toribio Frias').
+
+progenitor('Jesus Frias', 'Michelle Frias').
+progenitor('Lizbeth Hernandez', 'Michelle Frias').
+
+progenitor('Jesus Frias', 'Ingrid Frias').
+progenitor('Lizbeth Hernandez', 'Ingrid Frias').
+
+progenitor('Alvaro Frias', 'Christofer Frias').
+progenitor('Yeselin Zavaleta', 'Christofer Frias').
+
+progenitor('Alvaro Frias', 'Antony Frias').
+progenitor('Yeselin Zavaleta', 'Antony Frias').
+
+progenitor('Alvaro Frias', 'Zarate Frias').
+progenitor('Yeselin Zavaleta', 'Zarate Frias').
+
+progenitor('Bartolo Frias', 'Joaquin Frias').
+progenitor('Mariana Martinez', 'Joaquin Frias').
+
+progenitor('Bartolo Frias', 'Carlos Frias').
+progenitor('Mariana Martinez', 'Carlos Frias').
+
+progenitor('Toribio Frias', 'Eusebio Frias').
+progenitor('Ana Castilla', 'Eusebio Frias').
+
+progenitor('Toribio Frias', 'Maria Frias').
+progenitor('Ana Castilla', 'Maria Frias').
+
+// Yo
+progenitor('Rosa Garcia', 'Kevin Frias Garcia').
+progenitor('Sebastian Frias', 'Kevin Frias Garcia').
+
+// Hombre
+hombre('Alejandro Garcia').
+hombre('Mateo Garcia').
+hombre('Luis Sanchez').
+hombre('Pablo Nazaret').
+hombre('Guillermo Nazaret').
+hombre('Alejandro Camacho').
+hombre('Josue Elvira').
+hombre('Miguel Altamirano').
+hombre('Miguel Jr Altamirano').
+hombre('Maximiliano Frias').
+hombre('Tomas Frias').
+hombre('Fernando Frias').
+hombre('Sebastian Frias').
+hombre('Jesus Frias').
+hombre('Alvaro Frias').
+hombre('Bartolo Frias').
+hombre('Toribio Frias').
+hombre('Carlos Frias').
+hombre('Christofer Frias').
+hombre('Antony Frias').
+hombre('Zarate Frias').
+hombre('Joaquin Frias').
+hombre('Eusebio Frias').
+hombre('Kevin Frias Garcia').
+
+// Mujer
+mujer('Valentina Garcia').
+mujer('Isabella Escobar').
+mujer('Espinoza Garcia').
+mujer('Guadalupe Nazaret').
+mujer('Estrella Garcia').
+mujer('Sofia Sanchez').
+mujer('Griselda Garcia').
+mujer('Patricia Garcia').
+mujer('Alejandra Camacho').
+mujer('Pamela Camacho').
+mujer('Luz Camacho').
+mujer('Yutzil Camacho').
+mujer('Yuliana Elvira').
+mujer('Alexa Elvira').
+mujer('Sofia Altamirano').
+mujer('Elena Vega').
+mujer('Veronica Mendoza').
+mujer('Lizbeth Hernandez').
+mujer('Yeselin Zavaleta').
+mujer('Michelle Frias').
+mujer('Ingrid Frias').
+mujer('Clara Molina').
+mujer('Mariana Martinez').
+mujer('Ana Castilla').
+mujer('Maria Frias').
