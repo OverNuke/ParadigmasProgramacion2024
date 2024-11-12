c1e3eaf9757127e0527b9942686afe5276f3a550
OverNuke <ksfgarcia24@gmail.com>
2024-11-11 18:06:26 -0600

    Practica06 Actualizada

--- Prolog/S22017021_Practica_06.pl
+++ Prolog/S22017021_Practica_06.pl
@@ -1,6 +1,6 @@
 :- dynamic cliente/2.
 :- dynamic inventario/2.
-:- dynamic historial/2.
+:- dynamic historial/3.
 
 % Productos
 producto("tv",200).
@@ -50,61 +50,58 @@ inventario(Producto,Cantidad), Cantidad>0.
 
 % 2.- Comprar -------------------------------------------
 comprar(Cliente, Producto) :-
-% Comprabamos inventario & Saldo del cliente
-puede_comprar(Cliente,Producto)
-
-% 3.2 Mejorar comprar
-, \+hay_inventario(Producto) 
-, (write("No hay inventario.")) 
-; hay_inventario(Producto)
-, (
-% Obtengo los datos del producto & Cliente para almacenar
-cliente(Cliente,Saldo)
+% En caso de no contar con inventario
+(\+hay_inventario(Producto) ->
+format("No hay inventario!!!"))
+% Hay inventario ¿ Tiene el cliente suficiente para comprar?
+; (\+puede_comprar(Cliente, Producto) ->
+format("No cuentas con suficiente saldo para comprar el producto."))
+% Obtenemos los datos del cliente ahora que sabemos que puede comprar y existen unidades
+; cliente(Cliente,Saldo)
 , inventario(Producto,Cantidad)
 , producto(Producto,Precio)
-
 , NuevoSaldo is Saldo-Precio
 , NuevaCantidad is Cantidad-1
-
-% Aplicamos cambios
+% Aplicamos los datos a los hechos de cliente e inventario
 , retract(cliente(Cliente,Saldo))
 , assert(cliente(Cliente,NuevoSaldo))
 , retract(inventario(Producto,Cantidad))
 , assert(inventario(Producto,NuevaCantidad))
-
-% Actualizamos el historial de compra
-, assert(historial(Cliente,Producto))
-
-% Mensaje de confirmacion
-, writef("El cliente %w tiene un nuevo saldo de %w.\n", [Cliente, NuevoSaldo])
-, writef("El producto ahora tiene un stock %wpz.\n", [NuevaCantidad])).
+%Mensajes de confirmacion
+, writef("El cliente %w tiene un saldo de %w.\n", [Cliente, NuevoSaldo])
+, writef("El producto %w tiene un stock %wpz.\n", [Producto,NuevaCantidad])
+, (historial(Cliente,Producto,UnidadesAdquiridas) ->
+NuevaUnidad is UnidadesAdquiridas+1
+, retract(historial(Cliente,Producto,UnidadesAdquiridas))
+, assert(historial(Cliente,Producto,NuevaUnidad)))
+; assert(historial(Cliente,Producto,1)).
 
 
 % 3.1 Devolver -----------------------------------------
 devolver(Cliente,Producto) :-
 % Comprobamos que ha comprado dicho producto aqui
-\+historial(Cliente,Producto)
-, format("Lo sentimos usted no ha adquirido dicho producto en nuestra tienda.")
-; historial(Cliente,Producto)
-, (
+(\+historial(Cliente,Producto,UnidadesAdquiridas) ->
+format("Lo sentimos usted no ha adquirido dicho producto en nuestra tienda."))
+; historial(Cliente,Producto,UnidadesAdquiridas)
 % Obtengo los datos del producto & Cliente para almacenar
-cliente(Cliente,Saldo)
+,cliente(Cliente,Saldo)
 , inventario(Producto,Cantidad)
 , producto(Producto,Precio)
 
 , NuevoSaldo is Saldo+Precio
 , NuevaCantidad is Cantidad+1
+, NuevaUnidad is UnidadesAdquiridas-1
 
 % Aplicamos cambios
 , retract(cliente(Cliente,Saldo))
 , assert(cliente(Cliente,NuevoSaldo))
 , retract(inventario(Producto,Cantidad))
 , assert(inventario(Producto,NuevaCantidad))
-, retract(historial(Cliente,Producto))
+, retract(historial(Cliente,Producto,UnidadesAdquiridas))
+, assert(historial(Cliente,Producto,NuevaUnidad))
 
 % Mensaje de confirmacion
-, format("El producto ~w ha sido devuelto, ahora cuenta con un inventario de ~wpz.~nFue adquirido por ~w, quien cuenta ahora con un saldo de ~w.", [Producto,NuevaCantidad,Cliente,NuevoSaldo])
-).
+, format("El producto ~w ha sido devuelto, ahora cuenta con un inventario de ~wpz.~nFue adquirido por ~w, quien cuenta ahora con un saldo de ~w.", [Producto,NuevaCantidad,Cliente,NuevoSaldo]).
 
 % 3.3 Consultas -------------------------------------------
 consulta_inventario(Producto) :-
@@ -118,7 +115,7 @@ cliente(Cliente,Saldo)
 % 3.4 Rango de precios ----------------------------------------
 consultar_precio(Min,Max) :-
 producto(Producto,Precio)
-, Precio>=PrecioMin, Precio=<PrecioMax
+, Precio>=Min, Precio=<Max
 , format('~w~n',[Producto])
 , fail.
 consultar_precio(_,_).
